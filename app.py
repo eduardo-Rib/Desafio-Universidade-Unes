aea861635eb4b27383fe18745c110fd217eab460
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-05-10 15:41:43 -0300

    Atualizando css e linkando as paginas de unidades

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -16,6 +15,17 @@ def contatos():
     return render_template('contatos.html')
 
 
+@app.route('/campus1')
+def unidade1():
+    return render_template('unidade1.html')
 
+@app.route('/campus2')
+def unidade2():
+    return render_template('unidade2.html')
+
+@app.route('/campus3')
+def unidade3():
+     return render_template('unidade3.html')
+       
 if __name__ == "__main__":
     app.run(debug=True)
